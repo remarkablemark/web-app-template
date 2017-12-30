27e5a96ee47e8da2c7950c6d7ee2a67633f4e67d
Mark <mark@remarkablemark.org>
2017-12-30 17:27:12 -0500

    feat: update index page with welcome text and link to source
    Design inspired by express-generator:
    https://github.com/expressjs/generator

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-document.getElementById('app').innerHTML = '<h1>Hello, world!</h1>';
+document.getElementById('app').innerHTML = `
+  <h1>Welcome to <code>web-app-template</code>.</h1>
+  <a href="https://github.com/remarkablemark/web-app-template">
+    View source
+  </a>
+`;
+
 registerServiceWorker();
