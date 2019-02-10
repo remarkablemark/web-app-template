9b76d024c7004e8d26ec309f32ca19390ac7051a
Mark <mark@remarkablemark.org>
2019-02-10 14:58:59 -0500

    feat(index): make the "View Source" link open in a new tab

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 document.getElementById('app').innerHTML = `
   <h1>Welcome to <code>web-app-template</code>.</h1>
-  <a href="https://github.com/remarkablemark/web-app-template">
-    View source
+  <a href="https://github.com/remarkablemark/web-app-template" target="_blank">
+    View Source
   </a>
 `;
