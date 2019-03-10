3f53ba8093a33f4d58b45e77c97ac1a3ebb7a6b4
Mark <mark@remarkablemark.org>
2019-03-10 00:18:31 -0500

    feat(src): comment out serviceWorker and add rel attribute to link

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './index.css';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 document.getElementById('app').innerHTML = `
   <h1>Welcome to <code>web-app-template</code>.</h1>
-  <a href="https://github.com/remarkablemark/web-app-template" target="_blank">
+  <a href="https://github.com/remarkablemark/web-app-template" target="_blank" rel="noopener noreferrer">
     View Source
   </a>
 `;
@@ -11,4 +11,4 @@ document.getElementById('app').innerHTML = `
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
