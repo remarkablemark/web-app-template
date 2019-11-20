f42d741c402ea00d114656b511b71d462ff95370
Mark <mark@remarkablemark.org>
2019-11-20 00:03:51 -0500

    build(config): disable eslint-loader rules to make development easier

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -351,6 +351,9 @@ module.exports = function(webpackEnv) {
                   ? {
                       'no-console': 0,
                       'no-debugger': 0,
+                      'no-unused-vars': 0,
+                      'prettier/prettier': 0,
+                      'sort-imports': 0,
                     }
                   : {},
                 resolvePluginsRelativeTo: __dirname,
